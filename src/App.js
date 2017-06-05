5cf0923e80237cc6ef51552368af896c7ddee0cc
sDemon <novak.knutd@gmail.com>
2017-06-06 00:12:00 +0300

    lesson 12

--- src/App.js
+++ src/App.js
@@ -1,27 +1,45 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {TodoForm, TodoList} from './components/todo'
+import {TodoForm, TodoList} from './components/todo';
+import {addTodo, generateId} from './lib/todoHelpers'
 
 class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      todos: [
-        {id:1, name: "Learn JSX", iscomplete:true},
-        {id:2, name: "Build App", iscomplete:false},
-        {id:3, name: "Ship it!", iscomplete:false},
-      ],
-      currentTodo: ""
-    }
-    this.handelInputChange = this.handelInputChange.bind(this)
+  state = {
+    todos: [
+      {id:1, name: "Learn JSX", iscomplete:true},
+      {id:2, name: "Build App", iscomplete:false},
+      {id:3, name: "Ship it!", iscomplete:false},
+    ],
+    currentTodo: ''
   }
-  handelInputChange (event){
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    const newId = generateId()
+    const newTodo = {id: newId, name: this.state.currentTodo, isComplete: false}
+    const updatedTodos = addTodo(this.state.todos, newTodo)
+    this.setState({
+      todos: updatedTodos,
+      currentTodo: '',
+      errorMessage: ''
+    })
+  }
+
+  handleEmptySubmit = (event) => {
+    event.preventDefault()
+    this.setState({
+      errorMessage: 'Please supply a todo name'
+    })
+  }
+
+  handleInputChange = (event) => {
     this.setState({
       currentTodo: event.target.value
       })
   }
   render() {
+    const submitHandler = this.state.currentTodo ? this.handleSubmit : this.handleEmptySubmit;
     return (
       <div className="App">
         <div className="App-header">
@@ -29,7 +47,9 @@ class App extends Component {
           <h2>React TODO</h2>
         </div>
         <div className="Todo-App">
-          <TodoForm handelInputChange={this.handelInputChange} currentTodo={this.state.currentTodo}/>
+          {this.state.errorMessage && <span className='error'>{this.state.errorMessage}</span>}
+          <TodoForm handleInputChange={this.handleInputChange} currentTodo={this.state.currentTodo}
+          handleSubmit={submitHandler}/>
           <TodoList todos={this.state.todos}/>
         </div>
       </div>
--- src/components/todo/todoForm.js
+++ src/components/todo/todoForm.js
@@ -1,6 +1,13 @@
-import React from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
 
-export const TodoForm = (props) => (        <form>
-          <input type="text" onChange={props.handelInputChange} value={props.currentTodo}/>
+export const TodoForm = (props) => (        <form onSubmit={props.handleSubmit}>
+          <input type="text" onChange={props.handleInputChange} value={props.currentTodo}/>
         </form>
 )
+
+TodoForm.propTypes = {
+  currentTodo: PropTypes.string.isRequired,
+  handleInputChange: PropTypes.func.isRequired,
+  handleSubmit: PropTypes.func.isRequired
+}
