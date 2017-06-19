05b181d1c01ffefc5c78c6f472fb1dfb583ce1dd
sDemon <novak.knutd@gmail.com>
2017-06-19 15:04:15 +0300

    lesson 15

--- src/App.js
+++ src/App.js
@@ -2,16 +2,29 @@ import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {TodoForm, TodoList} from './components/todo';
-import {addTodo, generateId} from './lib/todoHelpers'
+import {addTodo, generateId, findById, toggleTodo, updateTodo, removeTodo} from './lib/todoHelpers'
+import {pipe, partial} from './lib/utils'
 
 class App extends Component {
   state = {
     todos: [
-      {id:1, name: "Learn JSX", iscomplete:true},
-      {id:2, name: "Build App", iscomplete:false},
-      {id:3, name: "Ship it!", iscomplete:false},
+      {id:1, name: "Learn JSX", isComplete:true},
+      {id:2, name: "Build App", isComplete:false},
+      {id:3, name: "Ship it!", isComplete:false},
     ],
     currentTodo: ''
+  }
+
+  handleRemove = (id, event) => {
+    event.preventDefault()
+    const updatedTodos = removeTodo(this.state.todos, id)
+    this.setState({todos: updatedTodos})
+  }
+
+  handleToggle = (id) => {
+    const getUpdatedTodos = pipe(findById, toggleTodo, partial(updateTodo, this.state.todos))
+    const updatedTodos = getUpdatedTodos(id, this.state.todos)
+    this.setState({todos: updatedTodos})
   }
 
   handleSubmit = (event) => {
@@ -50,7 +63,7 @@ class App extends Component {
           {this.state.errorMessage && <span className='error'>{this.state.errorMessage}</span>}
           <TodoForm handleInputChange={this.handleInputChange} currentTodo={this.state.currentTodo}
           handleSubmit={submitHandler}/>
-          <TodoList todos={this.state.todos}/>
+        <TodoList handleToggle={this.handleToggle} todos={this.state.todos} handleRemove={this.handleRemove}/>
         </div>
       </div>
     );
--- src/components/todo/todoItem.js
+++ src/components/todo/todoItem.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {partial} from '../../lib/utils'
 
 export const TodoItem = (props) => {
+  const handleToggle = partial(props.handleToggle, props.id)
+  const handleRemove = partial(props.handleRemove, props.id)
   return (
-    <li ><input type="checkbox" defaultChecked={props.iscomplete} />{props.name}</li>
+    <li >
+      <span className='delete-item'><a href="#" onClick={handleRemove}>x</a></span>
+      <input type="checkbox" onChange={handleToggle} checked={props.isComplete} />{props.name}</li>
   )
 }
 
